516b16d01e1cafe08a7b381e4223e30a6b5b96a9
SammokKabasi <sammokwhoelse@gmail.com>
2016-04-05 01:14:19 -0400

    Created a myvm that has nothing. This commit will compile but won't boot

--- kern/include/vm.h
+++ kern/include/vm.h
@@ -44,6 +44,11 @@
 #define VM_FAULT_WRITE       1    /* A write was attempted */
 #define VM_FAULT_READONLY    2    /* A write to a readonly page was attempted*/
 
+//added by sammokka
+struct coremap {
+
+};
+
 
 /* Initialization function */
 void vm_bootstrap(void);
---
+++ kern/vm/myvm.c
@@ -0,0 +1,105 @@
+/*
+ * Copyright (c) 2000, 2001, 2002, 2003, 2004, 2005, 2008, 2009
+ *	The President and Fellows of Harvard College.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE UNIVERSITY AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE UNIVERSITY OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <types.h>
+#include <kern/errno.h>
+#include <lib.h>
+#include <spl.h>
+#include <cpu.h>
+#include <spinlock.h>
+#include <proc.h>
+#include <current.h>
+#include <mips/tlb.h>
+#include <addrspace.h>
+#include <vm.h>
+
+/*
+ * Dumb MIPS-only "VM system" that is intended to only be just barely
+ * enough to struggle off the ground. You should replace all of this
+ * code while doing the VM assignment. In fact, starting in that
+ * assignment, this file is not included in your kernel!
+ *
+ * NOTE: it's been found over the years that students often begin on
+ * the VM assignment by copying dumbvm.c and trying to improve it.
+ * This is not recommended. dumbvm is (more or less intentionally) not
+ * a good design reference. The first recommendation would be: do not
+ * look at dumbvm at all. The second recommendation would be: if you
+ * do, be sure to review it from the perspective of comparing it to
+ * what a VM system is supposed to do, and understanding what corners
+ * it's cutting (there are many) and why, and more importantly, how.
+ */
+
+/* under dumbvm, always have 72k of user stack */
+/* (this must be > 64K so argument blocks of size ARG_MAX will fit) */
+#define DUMBVM_STACKPAGES    18
+
+/*
+ * Wrap ram_stealmem in a spinlock.
+ */
+//static struct spinlock stealmem_lock = SPINLOCK_INITIALIZER;
+
+void vm_bootstrap(void) {
+	/* Do nothing. */
+}
+
+
+/* Allocate/free some kernel-space virtual pages */
+vaddr_t alloc_kpages(unsigned npages) {
+	(void) npages;
+	return 0;
+}
+
+void free_kpages(vaddr_t addr) {
+	/* nothing - leak the memory. */
+
+	(void) addr;
+}
+
+unsigned
+int coremap_used_bytes() {
+
+	/* dumbvm doesn't track page allocations. Return 0 so that khu works. */
+
+	return 0;
+}
+
+void vm_tlbshootdown_all(void) {
+	panic("dumbvm tried to do tlb shootdown?!\n");
+}
+
+void vm_tlbshootdown(const struct tlbshootdown *ts) {
+	(void) ts;
+	panic("dumbvm tried to do tlb shootdown?!\n");
+}
+
+int vm_fault(int faulttype, vaddr_t faultaddress) {
+	(void) faulttype;
+	(void) faultaddress;
+	return 0;
+}
